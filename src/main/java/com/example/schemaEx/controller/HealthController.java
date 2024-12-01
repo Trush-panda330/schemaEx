bdbccb7394bb365fb135212d97950468afca0ccc
uetak330 <uetak330@gmail.com>
2024-12-01 18:43:19 +0900

    HealthControllerの実装
    api-schemaにより自動生成されたHealthControllerをimplementsして
    メソッドをオーバーライドして
    単純なResponseEntityを返して動作確認をしました。

--- src/main/java/com/example/schemaEx/controller/HealthController.java
+++ src/main/java/com/example/schemaEx/controller/HealthController.java
@@ -1,10 +1,13 @@
 package com.example.schemaEx.controller;
 
 import com.example.todoapi.controller.HealthApi;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController implements HealthApi {
-
-
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
