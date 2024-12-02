be90f05a70b0dbd0c3e0ad59d5a7e910645957b9
uetak330 <uetak330@gmail.com>
2024-12-02 17:47:13 +0900

    insert()の定義

--- src/main/java/com/example/schemaEx/repository/task/TaskRepository/TaskRepository.java
+++ src/main/java/com/example/schemaEx/repository/task/TaskRepository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.example.schemaEx.repository.task.TaskRepository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ public interface TaskRepository {
 
     @Select("select id, title from tasks where id = #{taskId}")
     Optional<TaskRecord> select(long taskId);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into tasks (title) values (#{title})")
+    void insert(TaskRecord record);
 }
